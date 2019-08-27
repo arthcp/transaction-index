af3b309ef4afd03962d727482c8fa5544e22d885
Arth C Patel <arthcp@ymail.com>
2019-08-27 10:49:02 +0530

    Add main script for running REST server

---
+++ src/api.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const service = require('./apiService');
+const config = require('../config.json');
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/getUserTransactions', async (req, res) => {
+    try {
+        if (req.body && req.body.userAddress) {
+            const userAddress = req.body.userAddress;
+            const response = await service.getUserTransactions({userAddress}); 
+            res.status(200).json({status: "success", data: response});
+            return;
+
+        } else {
+            console.error("Illegal arguments in getUserTransactions ", req.body);
+            res.status(400).json({status: "error"});
+            return;
+        }
+
+    } catch (e) {
+        console.error("API error", e);
+        res.status(500).json({status: "error"});
+        return;
+    }
+});
+
+app.all('*', async (req, res) => {
+    console.error("Wrong request");
+    res.status(404).json({status: "error"});
+    return;
+});
+
+app.listen(config.serverPort, () => {
+    console.info(`Listening on port ${config.serverPort}`);
+});
